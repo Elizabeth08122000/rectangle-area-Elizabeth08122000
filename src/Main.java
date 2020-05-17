d13aeec3dfe1fc0ce2191ee87f55c96c56ab6da3
gigi1988b <boris.gigi@gmail.com>
2020-05-18 01:40:04 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner sc = new Scanner(System.in);
-        double x1 = sc.nextDouble();
-        double x2 = sc.nextDouble();
-        double y1 = sc.nextDouble();
-        double y2 = sc.nextDouble();
-        double a = Math.abs(x2 - x1);
-        double b = Math.abs(y2 - y1);
-        System.out.println(a * b);
-        System.out.println(Float.MAX_VALUE);
     }
 }
